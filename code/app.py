d335e170631533932b22b48b5848c8e5ab29313e
XuanWANG <n830026114@mail.uic.edu.cn>
2021-02-23 16:21:30 +0800

    Modify hello() method and add new add_user() method

--- code/app.py
+++ code/app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route("/add")
+def add_user():
+    # FIXME: Add new user code logic here
+    return jsonify({
+        'nickname':'uic02',
+        'email':'12142819@qq.com',
+        'role':'developer',
+        'code':200
+    })
